85cd538e86062b4775fd9a824f45a8363e872886
cirkovic <predrag.cirkovic@cern.ch>
2017-04-21 16:43:09 +0200

    Initial code for rescaling the input root files.

---
+++ batch_script_4.sh
@@ -0,0 +1,16 @@
+N=${1}
+M=${2}
+I1=${3}
+I2=${4}
+J1=${5}
+J2=${6}
+OUT=${7}
+
+cd /afs/cern.ch/work/c/cirkovic/kLimits/CombineHarvester/CMSSW_7_4_7/src/
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/work/c/cirkovic/FCNC_limits/rescaling
+
+COMMAND="python rscript_4.py ${N} ${M} ${I1} ${I2} ${J1} ${J2} ${OUT}"
+echo $COMMAND
+eval $COMMAND
+
---
+++ run_5.sh
@@ -0,0 +1,72 @@
+#rm -rf output/*
+
+OUTPUT=rescaling_16-04-2017_2
+OUTPUT=rescaling_16-04-2017_4
+OUTPUT=rescaling_17-04-2017_4
+OUTPUT=rescaling_18-04-2017_5
+
+#N=50
+#N=100
+#N=20
+N=50
+
+NJOBS=5
+#NJOBS=10
+
+M=0.2
+M=0.5
+M=1.0
+M=${1}
+
+EOS=/eos/cms/store/caf/user/mdjordje/Cirkovic
+OUT="${EOS}/${OUTPUT}_${N}_${M}"
+
+eos rm -r ${OUT}
+eos mkdir ${OUT}
+
+for i in `seq 0 ${N}`; do
+    for j in `seq 0 ${N}`; do
+        eos mkdir ${OUT}/${i}_${j}
+    done
+done
+
+#BSUB_QUIET=1
+N1=$((N-1))
+N2=$((N1/NJOBS))
+
+#for i in `seq 0 $((N-1))`; do
+for i in `seq 0 $N2`; do
+#for i in `seq 10 10`; do
+    #for j in `seq 0 $((N-1))`; do
+    for j in `seq 0 $N2`; do
+    #for j in `seq 25 25`; do
+#        I1=$((i * 1))
+#        J1=$((j * 1))
+#        I2=$((I1 + 1))
+#        J2=$((J1 + 1))
+#        if [[ "$i" == "$((N-1))" ]]; then I2=$((I1 + 2)); fi
+#        if [[ "$j" == "$((N-1))" ]]; then J2=$((J1 + 2)); fi
+        I1=$((i * NJOBS))
+        J1=$((j * NJOBS))
+        I2=$((I1 + NJOBS))
+        J2=$((J1 + NJOBS))
+        if [[ "$i" == "$N2" ]]; then I2=$((I1+NJOBS+1)); fi
+        if [[ "$j" == "$N2" ]]; then J2=$((J1+NJOBS+1)); fi
+        #COMMAND="python rscript.py 100 ${I1} ${I2} ${J1} ${J2}"
+        #COMMAND="bsub -q 1nh batch_script.sh 50 ${I1} ${I2} ${J1} ${J2}"
+        #COMMAND="bsub -q 1nh batch_script.sh ${N} ${I1} ${I2} ${J1} ${J2}"
+        #COMMAND="bsub -q 1nh batch_script.sh ${N} ${M} ${I1} ${I2} ${J1} ${J2}"
+        #COMMAND="bsub -q 1nh -o /dev/null batch_script.sh ${N} ${M} ${I1} ${I2} ${J1} ${J2}"
+#        COMMAND="bsub -q 1nh -o /tmp/cirkovic/job_out batch_script.sh ${N} ${M} ${I1} ${I2} ${J1} ${J2}"
+        #COMMAND="sh batch_script.sh ${N} ${M} ${I1} ${I2} ${J1} ${J2}"
+#        echo $COMMAND
+#        eval $COMMAND
+        #bsub -q 1nh -o /tmp/cirkovic/job_out batch_script.sh ${N} ${M} ${I1} ${I2} ${J1} ${J2}
+        #COMMAND="bsub -q 8nh -o /tmp/cirkovic batch_script_2.sh ${N} ${M} ${I1} ${I2} ${J1} ${J2} ${OUT}"
+        COMMAND="bsub -q 1nh -o /tmp/cirkovic batch_script_5.sh ${N} ${M} ${I1} ${I2} ${J1} ${J2} ${OUT}"
+        echo $COMMAND
+        eval $COMMAND
+        #exit
+    done
+done
+
